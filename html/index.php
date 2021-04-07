ff96dcab7d6920cdb8773c3c48985de0c0579ab1
Romulo <romfe89@gmail.com>
2021-04-07 17:29:19 -0300

    added HTML files

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <div class="container">
+            <h1>Product Stock Manager</h1>
+            <p>Use this form to read, create, delete and update records</p>
+        </div>
+    </body>
+</html>
