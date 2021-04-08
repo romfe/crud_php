31c7a7a2c4112c20161c223e213f9de80c1868d5
romfe <romfe89@gmail.com>
2021-04-08 15:29:15 -0300

    updated main page and added file upload logic

--- html/create.php
+++ html/create.php
@@ -1,7 +1,8 @@
 <?php // file that makes inputs to the database
     include '../db.php';
     function invalidInput($variableName){
-        echo '<script>alert("Invalid input for ', $variableName, ' variable")</script>';
+        echo '<script>alert("Invalid input for $variableName variable. Please check the imput rules and try again.")</script>';
+        header("Location: index.php");
         exit(0);
     }
     $title = $_POST["title"];
@@ -17,6 +18,26 @@
     }
     if(isset($stock) === FALSE){
         invalidInput("Stock");
+    }
+    if(isset($_POST['submit']) and isset($_FILES['product_image'])){
+        $img_name = $_FILES['product_image']['name']; //getting the image name
+        $img_size = $_FILES['product_image']['size']; //getting the image size
+        $tmp_name = $_FILES['product_image']['tmp_name']; 
+        $img_error = $_FILES['product_image']['error']; //getting the image error
+        $img_extension = pathinfo($img_name, PATHINFO_EXTENSION); // getting the image extension
+        $img_extension = strtolower($img_extension);
+        $allowed_extensions = array("jpg", "jpeg", "gif", "png");
+        if (($img_error === 0) and ($img_size <= 625000) and (in_array($img_extension, $allowed_extensions))){
+            
+            $new_img_name = uniqid("IMG-", true).'.'.$img_extension;
+            $upload_path = '../images/'.$new_img_name;
+            move_uploaded_file($tmp_name, $upload_path);
+            
+        }else{
+            invalidInput("Image");
+        }
+    }else{
+        invalidInput("Image");
     }
     
     
--- html/index.php
+++ html/index.php
@@ -6,11 +6,15 @@
     <body>
         <div class="container">
             <h1>Product Stock Manager</h1>
-            <p>Use this form to read, create, delete and update product records</p>
-            <p>Title must be more than 6 and less than 25 characters long</p>
-            <p>Description must be less than 4000 characters long</p>
-            <p>Stock is a required data</p>
-
+            <h3>Use this form to read, create, delete and update product records</h3>
+            <p><strong>Input rules:</strong></p>
+            <ul>
+                <li>Title must be more than 6 and less than 25 characters long</li>
+                <li>Description must be less than 4000 characters long</li>
+                <li>Stock is a required data</li>
+                <li>Image must have up to 5MB in size and in one of the following formats: .gif .jpg or .png</li>
+            </ul>
+            
             <form class="form-inline m-2" action="create.php" method="POST">
                 <label for="title">Title:</label>
                 <input type="text" class="form-control m-2" id="title" name="title"> 
