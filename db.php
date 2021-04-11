46f4520b72c3f63a3aead5ec3ed220448b0e00f7
Romulo <romfe89@gmail.com>
2021-04-11 09:55:27 -0300

    first test ready

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php
     //document that manages the database connection
-    $conn = new mysqli("localhost", "root", "toor", "crud_products");
-
+    // old stuff be like $conn = new mysqli("localhost", "root", "toor", "crud_products");
+    $sname = "localhost";
+    $uname = "root";
+    $password = "toor";
+    $db_name = "crud_products";
+    $conn = mysqli_connect($sname, $uname, $password, $db_name);
 ?>
